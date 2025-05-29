bd91d2eca08fbd612c48e9cf07583c1f0fb96606
manisha Sasmitha <manisha1718manisha@gmail.com>
2025-05-29 23:10:56 +0530

    🌀 ᴍᴀɴɪꜱʜᴀ-ᴍᴅ 💕

--- config.js
+++ config.js
@@ -6,33 +6,10 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || "svEWFJhY#js5-b5WOumgdchibUPJF21d634GYTYa2ZFeRkxxId_E",
-    CUSTOM_REACT: process.env.CUSTOM_REACT || "false",
-    CUSTOM_REACT_EMOJIS: process.env.CUSTOM_REACT_EMOJIS || "💝,💖,💗,❤️‍🔥,❤️‍🩹,❤️,🩷,🧡,💛,💚,💙,🩵,💜,🤎,🖤,🩶,🤍",
-    ALWAYS_ONLINE: process.env.ALWAYS_ONLINE || "false",
-    AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
-    READ_MESSAGE: process.env.READ_MESSAGE || "false", // Added auto-read configuration
-    AUTO_TYPING: process.env.AUTO_TYPING || "false",
-    ALWAYS_ONLINE: process.env.ALWAYS_ONLINE || "false",
-    CURRENT_STATUS: process.env.CURRENT_STATUS || "false",
+    SESSION_ID: process.env.SESSION_ID || "",
     MODE: process.env.MODE || "private",
-    AUTHOR: (process.env.PACK_INFO?.split(';') || [])[0] || 'MANISHA MD',
-    PACKNAME: (process.env.PACK_INFO?.split(';') || [])[1] || 'manisha',
-    AUTO_REPLY: process.env.AUTO_REPLY || "false",
-    ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/vbi10j.png",
-    ALIVE_MSG: process.env.ALIVE_MSG || "HII DEAR IM ONLINE I'M MANISHA-MD WHATSAPP BOT 😊♻️",
-    ANTI_LINK: process.env.ANTI_LINK || "false",
-    ANTI_BAD: process.env.ANTI_BAD || "false",
     PREFIX: process.env.PREFIX || "/",
-    FAKE_RECORDING: process.env.FAKE_RECORDING || "false",
-    AUTO_REACT: process.env.AUTO_REACT || "false",
-    OWNER_REACT: process.env.OWNER_REACT || "false",
-    ANTI_VV: process.env.ANTI_VV || "true",
-    ANTI_DEL_PATH: process.env.ANTI_DEL_PATH || "log", // change it to 'same' if you want to resend deleted message in same chat
-    DEV: process.env.DEV || "94721551183", //replace with your whatsapp number
-    AUTOLIKESTATUS: process.env.AUTOLIKESTATUS || "true",
-    AUTO_STATUS_REPLY: process.env.AUTO_STATUS_REPLY || "false",
-    AUTO_STATUS__MSG: process.env.AUTO_STATUS__MSG || "`YOUR STATUS SEEN JUST NOW BY MANISHA-MD`",
-    OMDB_API_KEY: process.env.OMDB_API_KEY || "76cb7f39", // omdbapi.com
+    ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/vbi10j.png",
+    ALIVE_MSG: process.env.ALIVE_MSG || "HII DEAR IM ONLINE I'M MANISHA-MD WHATSAPP BOT 😊♻️"
     };
     
