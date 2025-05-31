980562fa56d995ada86e312f51658aeba52c40bf
manisha Sasmitha <manisha1718manisha@gmail.com>
2025-05-31 16:10:02 +0530

    🌀 ᴍᴀɴɪꜱʜᴀ-ᴍᴅ 💕

--- config.js
+++ config.js
@@ -6,13 +6,12 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || "hud31TDK#H2OQhUVMMZkCPQnJ3obHXdFEF5qQhzU11z0ynU1nyNQ",
+    SESSION_ID: process.env.SESSION_ID || "",
     MODE: process.env.MODE || "private",
     PREFIX: process.env.PREFIX || "/",
     AUTHOR: (process.env.PACK_INFO?.split(';') || [])[0] || 'MANISHA MD',
     PACKNAME: (process.env.PACK_INFO?.split(';') || [])[1] || 'MANISHA MD',
     ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/vbi10j.png",
-    ALIVE_MSG: process.env.ALIVE_MSG || "HII DEAR IM ONLINE I'M MANISHA-MD WHATSAPP BOT 😊♻️",
     GEMINI_API_KEY: process.env.GEMINI_API_KEY || "AIzaSyBP9qAGQUHjtIPuaZcyvSnbZDGSyHUD6bc",
     MOVIE_API_KEY: process.env.MOVIE_API_KEY || "sky|decd54b4fa030634e54d6c87fdffbb95f0bb9fb5"
     };
